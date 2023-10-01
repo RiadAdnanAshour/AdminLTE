0530c625e362becea6c23464d8946f445eafff4d
RiadAdnanAshour <riadashour153@gmail.com>
2023-10-01 13:29:08 +0300

    end city

---
+++ resources/views/cms/cities/create.blade.php
@@ -0,0 +1,77 @@
+@extends('cms.parent')
+
+
+
+@section('title','Create City')
+@section('page-big-title','Create City')
+@section('page-main-title','cities')
+@section('page-sub-title','Create')
+@section('styles')
+@endsection
+
+
+@section('content')
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-6">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Create City</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        @if($errors->any())
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+                            @foreach($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach
+                            </div>
+                        @endif
+                        @if(session('message'))
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h5><i class="icon fas fa-check"></i> Alert!</h5>
+                            {{ session('message') }}
+                        </div>
+                        @endif
+                        <!-- form start -->
+                        <form method="post" action="{{route('cities.store')}}">
+                            @csrf
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="Name">Name</label>
+                                    <input type="text" name="name" value="{{old('name')}}" class="form-control" id="Name" placeholder="Enter Name">
+                                </div>
+
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+
+                    <!-- general form elements -->
+
+                    <!-- /.card -->
+
+                </div>
+                <!--/.col (left) -->
+
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
+
+
+@section('script')
+@endsection
